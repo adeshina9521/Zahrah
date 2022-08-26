763f9c2974b3582e6ee87e00a5d7413440608864
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-08-26 09:15:11 +0100

    first commit and signUp

---
+++ src/component/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function SignUp(){
+    return(
+        <div className="signup">
+            <div className="sgn-bg">
+                <h1>Sign up form</h1>
+            </div>
+        </div>
+    )
+}
