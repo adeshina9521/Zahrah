fb902ce8e095cf87b7e5eba071377acd4c3f712a
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-09-23 17:50:20 +0100

    card style

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,9 +4,12 @@ export default function Card(){
     return(
         <div className="main_card">
             <div className="card">
-                    <img src="../images/productsImages/hijab1.jpg" alt="hijab" className="hij_img"></img>
+                <p className="trans"> SOLD </p>
+                
+                <img src="../images/productsImages/hijab1.jpg" alt="hijab" className="hij_img"></img>
+                   
             </div>
-            <p className="trans">SOLD</p>
+            
         </div>
     )
 }
