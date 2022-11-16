04e8c21185363f1fb04998af07b8d080def52b2b
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-11-16 09:28:21 +0100

    footer styling

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,8 +5,25 @@ import React from "react"
 export default function Footer(){
     return(
         <div className="card-ft">
-            <img className="ft_logo lg" src="../images/twiter_logo.png" alt="twitterlogo"></img>
-            <img className="ft_logo " src="../images/facebook_logo.png" alt="facebooklogo"></img>
+            <div className="subcribe">
+                <h3 className="sub">Subscribe for new releases, promotions, hijab news and more:</h3>
+                <input
+                    className="newsletter"
+                    type="email"
+                    name="email"
+                    placeholder="email">                  
+                </input>
+                <button>Subscribe</button>
+            </div>
+
+            <div className="social">
+                <img className="ft_logo" src="../images/twiter_logo.png" alt="twitterlogo"></img>
+                <img className="ft_logo" src="../images/facebook_logo.png" alt="facebooklogo"></img>
+                <img className="ft_logo" src="../images/tiktok_logo.png" alt="tiktok logo"></img>
+                <img className="ft_logo" src="../images/youtube_logo.jpg" alt="youtube logo"></img>
+                <img className="ft_logo" src="../images/instagram_logo.png" alt="instagram logo"></img>
+            </div>
+            
             {/* <img className="ft_logo" src={Githublogo} alt="twiterlogo"></img> */}
         </div>
     )
