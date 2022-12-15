6ad6c8bd024f9cb53b22bff7f447dbfeff915846
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-12-15 15:25:57 +0100

    products data

---
+++ src/component/Product/NiqabData.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        id: 1,
+        style: "",
+        image: "../images/Hijab/hijab1.jpg",
+        price: "" ,
+        color: "blue",
+        Sleeve: 2 ,
+        openSpots: 0,
+
+
+    },
+    {
+        id: 2,
+        style: "",
+        image: "../images/Hijab/hijab2.jpg",
+        price: "" ,
+        color: "red",
+        Sleeve: 7 ,
+        openSpots: 2,
+    },
+    {
+        id: 3,
+        style: "",
+        image: "../images/Hijab/hijab3.jpg",
+        price: "" ,
+        color: "green",
+        Sleeve: 9 ,
+        openSpots:4,
+    },
+    {
+        id: 4,
+        style: "",
+        image: "../images/Hijab/hijab4.jpg",
+        price: "" ,
+        color: "yellow",
+        Sleeve: 12,
+        openSpots: 0,
+    },
+    {
+        id: 5,
+        style: "",
+        image: "../images/Hijab/hijab5.jpg",
+        price: "" ,
+        color: "yellow",
+        Sleeve: 12,
+        openSpots: 0,
+    },
+    {
+        id: 6,
+        style: "",
+        image: "../images/Hijab/hijab6.jpg",
+        price: "" ,
+        color: "yellow",
+        Sleeve: 12,
+        openSpots: 1,
+    },
+]
